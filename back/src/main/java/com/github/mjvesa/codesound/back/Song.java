1290fa1428b723182a0838ee1cd50402c5137c05
Matti Vesa <matti.vesa@mavericks.fi>
2022-09-23 15:46:47 +0300

    Initial commit

---
+++ back/src/main/java/com/github/mjvesa/codesound/back/Song.java
@@ -0,0 +1,20 @@
+package com.github.mjvesa.codesound.back;
+
+public class Song {
+
+	private final long id;
+	private final String content;
+
+	public Song(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
---
+++ back/src/main/java/com/github/mjvesa/codesound/back/SongController.java
@@ -0,0 +1,20 @@
+package com.github.mjvesa.codesound.back;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SongController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/song")
+	public Song song(@RequestParam(value = "id", defaultValue = "0") Long id) {
+		return new Song(counter.incrementAndGet(), String.format(template, name));
+	}
+}
